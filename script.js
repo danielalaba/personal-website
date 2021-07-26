98050cbff96699074016b41a3f80f245e92df516
danielalaba <ralphalaba@gmail.com>
2021-07-26 16:46:13 +0800

    Add dark mode button in desktop

---
+++ script.js
@@ -0,0 +1,5 @@
+var darkMode = document.getElementById("btnDark");
+ darkMode.addEventListener("click", function() {
+    var darkToggle = document.body;
+    darkToggle.classList.toggle("btnDark");
+});
